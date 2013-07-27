37543c9a7596397b917f4f62cf29b46f2e2149db
glori <glori@glori-Anabtawi.(none)>
2013-07-27 20:30:10 +0530

    first commit

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :title, :body, :name, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '79e3eea282d3f93dd9da1389a416d018fec4aca5402aa18fcf43c54f5bc959a8d1ae5f29402320472ba17e4fa2b2857e306b10bea88a302f9e8ae94d06dbf8b6'
